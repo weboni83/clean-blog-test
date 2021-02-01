8874e01242c83cca950af05a921ed1882635d295
NOTEBOOK-SOSOZ\webon <weboni83@gmail.com>
2021-02-01 15:29:45 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+bundle exec jekyll build
+
+# navigate into the build output directory
+cd _site
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/weboni83/weboni83.github.io.git main:docs
